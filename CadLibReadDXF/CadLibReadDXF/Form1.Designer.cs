e21edbb14fe6a3e6f1f6c4504a002950b8f210ec
TUTWO <TUTWO>
2018-01-27 19:43:34 +0800

    'CadLib'

---
+++ CadLibReadDXF/CadLibReadDXF/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace CadLibReadDXF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenDxfFile = new System.Windows.Forms.Button();
+            this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
+            this.xtraTabPage1 = new DevExpress.XtraTab.XtraTabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).BeginInit();
+            this.xtraTabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OpenDxfFile
+            // 
+            this.OpenDxfFile.Location = new System.Drawing.Point(15, 15);
+            this.OpenDxfFile.Name = "OpenDxfFile";
+            this.OpenDxfFile.Size = new System.Drawing.Size(83, 29);
+            this.OpenDxfFile.TabIndex = 0;
+            this.OpenDxfFile.Text = ". . .";
+            this.OpenDxfFile.UseVisualStyleBackColor = true;
+            this.OpenDxfFile.Click += new System.EventHandler(this.OpenDxfFile_Click);
+            // 
+            // xtraTabControl1
+            // 
+            this.xtraTabControl1.Location = new System.Drawing.Point(120, 15);
+            this.xtraTabControl1.Name = "xtraTabControl1";
+            this.xtraTabControl1.SelectedTabPage = this.xtraTabPage1;
+            this.xtraTabControl1.Size = new System.Drawing.Size(823, 568);
+            this.xtraTabControl1.TabIndex = 2;
+            this.xtraTabControl1.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
+            this.xtraTabPage1});
+            this.xtraTabControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.xtraTabPage1_Paint);
+            // 
+            // xtraTabPage1
+            // 
+            this.xtraTabPage1.Name = "xtraTabPage1";
+            this.xtraTabPage1.Size = new System.Drawing.Size(817, 539);
+            this.xtraTabPage1.Text = "xtraTabPage1";
+            this.xtraTabPage1.Paint += new System.Windows.Forms.PaintEventHandler(this.xtraTabPage1_Paint);
+            this.xtraTabPage1.Resize += new System.EventHandler(this.xtraTabPage1_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 595);
+            this.Controls.Add(this.xtraTabControl1);
+            this.Controls.Add(this.OpenDxfFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).EndInit();
+            this.xtraTabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenDxfFile;
+        private DevExpress.XtraTab.XtraTabControl xtraTabControl1;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage1;
+    }
+}
+
---
+++ CadLibReadDXF/CadLibReadDXF/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+
+using WW.Cad.Base;
+using WW.Cad.Drawing.GDI;
+using WW.Cad.IO;
+using WW.Cad.Model;
+using WW.Math;
+using WW.Cad.Drawing;
+//using DevExpress.XtraTab;
+
+namespace CadLibReadDXF
+{
+    public partial class Form1 : Form
+    {
+        private Matrix4D modelTransform = Matrix4D.Identity;
+        private GDIGraphics3D gdiGraphics3D;
+        private DxfModel model;//定义DxfModel对象  
+        private Bounds3D bounds;
+        private string filename;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateTo2DTransform()
+        {
+            if (bounds != null)
+            {
+                Matrix4D to2DTransform = DxfUtil.GetScaleTransform(
+                    bounds.Corner1,
+                    bounds.Corner2,
+                    bounds.Center,
+                    new Point3D(0d, ClientSize.Height, 0d),
+                    new Point3D(ClientSize.Width, 0d, 0d),
+                    new Point3D(ClientSize.Width / 2, ClientSize.Height / 2, 0d)
+                );
+                gdiGraphics3D.To2DTransform = to2DTransform * modelTransform;
+            }
+        }
+        private void OpenDxfFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "AutoCad files (*.dwg, *.dxf)|*.dxf;*.dwg";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                filename = dialog.FileName;
+            }
+            if (!string.IsNullOrEmpty(filename))
+            {
+
+                //this.xtraTabPage1.BackColor = System.Drawing.Color.Black;
+                try
+                {
+                    //通过文件扩展名判断CAD文件是dwg格式还是dxf格式  
+                    string extension = Path.GetExtension(filename);
+                    if (string.Compare(extension, ".dwg", true) == 0)
+                    {
+                        model = DwgReader.Read(filename);
+                    }
+                    else
+                    {
+                        model = DxfReader.Read(filename);
+                    }
+                    //将控件的标签添加上文件名  
+                    this.xtraTabPage1.Text = "二维仿真(" + Path.GetFileName(filename) + ")";
+                    //设置控件背景为黑色  
+                    this.xtraTabPage1.BackColor = System.Drawing.Color.Black;
+
+                    //使用GDIGraphics3D绘制CAD文件的方法  
+                    //创建中间可绘制对象  
+                    gdiGraphics3D = new GDIGraphics3D(GraphicsConfig.BlackBackgroundCorrectForBackColor);
+                    gdiGraphics3D.CreateDrawables(model);
+                    //获得bounding box  
+                    bounds = new Bounds3D();
+                    gdiGraphics3D.BoundingBox(bounds, modelTransform);
+                    //计算GDIGraphics3D的属性To2DTransform  
+                    CalculateTo2DTransform();
+                    //响应控件的Paint事件，画CAD文件  
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("文件有错！请用AutoCad打开，通过“文件-核查”尝试修复。错误信息：" + ex.Message);
+                }
+            }
+        }
+        //xtraTabPage3控件的Paint事件，画CAD文件  
+        private void xtraTabPage1_Resize(object sender, EventArgs e)
+        {
+            base.OnResize(e);
+            CalculateTo2DTransform();
+            this.xtraTabPage1.Invalidate();
+        }
+
+        private void xtraTabPage1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            if (gdiGraphics3D != null)
+            {
+                gdiGraphics3D.Draw(e.Graphics, xtraTabPage1.ClientRectangle);
+            }
+        }
+    }
+}
